83ad43a45fcee25ef446dfee26b2b383f8e99d1d
Rachel Wagschal <rawagschal@gmail.com>
2021-01-16 00:20:23 -0800

    added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Contact from './components/Contact';
 import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
@@ -65,6 +66,7 @@ const [currentCategory, handlePageChange] = useState();
           <div>{renderPage(currentCategory)}</div>
         </Switch>
       </main>
+      <Footer/>
     </div>
     </Router>
   );
---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <ul>
+                <li><a href="https://github.com/rawagschal" target="_blank" rel="noreferrer">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/rachelwag" target="_blank" rel="noreferrer">Linkedin</a></li>
+                <li><a href="https://stackoverflow.com/users/14019495/rachel-wagschal" target="_blank" rel="noreferrer">Stack Overflow</a></li>
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer;
