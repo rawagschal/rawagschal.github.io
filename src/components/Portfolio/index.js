532404dcb7cd3510b9743e442c3d1f35df012a50
Rachel Wagschal <rawagschal@gmail.com>
2021-02-07 22:14:02 -0800

    added petter life and app/repo links

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 
-function Portfolio({currentProject}) {
+function Portfolio() {
     // const { name, description } = currentProject;
 
-    const [projects] = useState([
+    const projects = [
+         {
+          name: 'Notetakr',
+          description: 'Express.js',
+          deployedURL: 'https://arcane-wave-64105.herokuapp.com/',
+          githubURL: 'https://github.com/rawagschal/notetakr',
+        },
         {
           name: 'Spaced Out',
           description: 'HTML/Bootstrap/JS',
@@ -11,27 +17,33 @@ function Portfolio({currentProject}) {
           githubURL: 'https://github.com/rawagschal/spaced-out',
         },
         {
-          name: 'Notetakr',
-          description: 'Express.js',
-          deployedURL: 'https://arcane-wave-64105.herokuapp.com/',
-          githubURL: 'https://github.com/rawagschal/notetakr',
-        },
-    ]);
+          name: 'Petter Life',
+          description: 'MERN App',
+          deployedURL: 'https://a-petter-life.herokuapp.com/',
+          githubURL: 'https://github.com/ChrisMeissner/petter-life',
+        }
+    ];
 
-    
 
     return (
         <section>
-        <h1>{(projects.name)}</h1>
-        <p>{projects.description}</p>
+        <h1>My Work</h1>
+        <p>Some of my most recent projects:</p>
         <div className="flex-row">
                 {projects.map((image, i) => (
-                    <img
-                        src={require(`../../assets/projects/${i}.png`).default}
-                        alt={image.name}
-                        className="img-thumbnail mx-1"
-                        key={image.name}
-                    />
+                    <div className="project-container">
+                        <div className="project-header">
+                            <h3 className="project-title">{projects[i].name}</h3>
+                            <a href={projects[i].deployedURL} className="deployed-link">App</a>
+                            <a href={projects[i].githubURL} className="repo-link">Github Repository</a>
+                        </div>
+                        <img
+                            src={require(`../../assets/projects/${i}.png`).default}
+                            alt={image.name}
+                            className="img-thumbnail mx-1"
+                            key={image.name}
+                        />
+                    </div>
                 ))}
             </div>
         </section>
