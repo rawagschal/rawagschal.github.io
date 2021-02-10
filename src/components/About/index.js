a76e192b544838b6a34f34ba1062d0cc7c7ec561
Rachel Wagschal <rawagschal@gmail.com>
2021-02-10 15:58:59 -0800

    styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 
 function About() {
     return (
-        <section className="my-5">
-            <h1 id="about">About Rachel</h1>
-            <p> Rachel is a junior web developer learning to share beautiful and useful information
-                through code. 
-            </p>
+        <section className="about-container">
+            <div className="about-col-1">
+                <h2 id="about" className="about-header">Hello, World!</h2>
+                <p> 
+                    Meet Rachel, a junior full stack web developer with
+                    a passion for problem solving via creative solutions. 
+                </p>
+            </div>
             <img src={aboutImage} className="my-2" style={{ width: "45% "}} alt="Rachel Wagschal on her graduation day from the University of Oregon in June 2019." />
         </section>
     );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import ghLogo from '../../assets/img/ghLogo32.png';
-import liLogo from '../../assets/img/liLogo.png'
-import soLogo from '../../assets/img/soLogo.png'
+
 
 function Footer() {
     return (
         <footer>
             <ul className="footer-items">
-                <li><a href="https://github.com/rawagschal" target="_blank" rel="noreferrer">
-                    <img src={ghLogo} alt="Github" className="gh-logo" />
-                </a></li>
-                <li><a href="https://www.linkedin.com/in/rachelwag" target="_blank" rel="noreferrer">
-                    <img src={liLogo} alt="Linkedin" className="li-logo" />
-                </a></li>
-                <li><a href="https://stackoverflow.com/users/14019495/rachel-wagschal" target="_blank" rel="noreferrer">
-                     <img src={soLogo} alt="Stack Overflow" className="so-logo" />
-                </a></li>
+                <li>
+                    <a href="https://github.com/rawagschal" target="_blank" rel="noreferrer">
+                    Github
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/rachelwag" target="_blank" rel="noreferrer">
+                    Linkedin
+                    </a>
+                </li>
+                <li>
+                    <a href="https://stackoverflow.com/users/14019495/rachel-wagschal" target="_blank" rel="noreferrer">
+                    Stack Overflow
+                    </a>
+                </li>
             </ul>
         </footer>
     )
