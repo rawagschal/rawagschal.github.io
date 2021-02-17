31fd2b14e6efc1ea964a855d2073bceee95157ef
Rachel Wagschal <rawagschal@gmail.com>
2021-02-16 16:07:59 -0800

    basic portfolio style

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,11 +7,12 @@ function About() {
             <div className="about-col-1">
                 <h2 id="about" className="about-header">Hello, World!</h2>
                 <p> 
-                    Meet Rachel, a junior full stack web developer with
-                    a passion for problem solving via creative solutions. 
+                    Meet Rachel, a junior full-stack web developer with
+                    a desire to create humane techonologies for more meaningful 
+                    engagement on the web. 
                 </p>
             </div>
-            <img src={aboutImage} className="my-2" style={{ width: "45% "}} alt="Rachel Wagschal on her graduation day from the University of Oregon in June 2019." />
+            <img src={aboutImage} className="about-img" alt="Rachel Wagschal on her graduation day from the University of Oregon in June 2019." />
         </section>
     );
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -26,26 +26,30 @@ function Portfolio() {
 
 
     return (
-        <section>
-        <h1>My Work</h1>
-        <p>Some of my most recent projects:</p>
-        <div className="flex-row">
+        <section className="portfolio-container">
+        <div className="portfolio-header">
+            <h2>My Work</h2>
+            <p>Some of my most recent projects:</p>
+        </div>
+        <div className="grid-container">
                 {projects.map((image, i) => (
                     <div className="project-container">
                         <div className="project-header">
                             <h3 className="project-title">{projects[i].name}</h3>
-                            <a href={projects[i].deployedURL} className="deployed-link">App</a>
-                            <a href={projects[i].githubURL} className="repo-link">Github Repository</a>
                         </div>
                         <img
                             src={require(`../../assets/projects/${i}.png`).default}
                             alt={image.name}
-                            className="img-thumbnail mx-1"
+                            className="project-img"
                             key={image.name}
                         />
+                        <div className="project-footer">
+                            <a href={projects[i].deployedURL} className="deployed-link">App</a>
+                            <a href={projects[i].githubURL} className="repo-link">Github Repository</a>
+                        </div>
                     </div>
                 ))}
-            </div>
+        </div>
         </section>
     );
     }
