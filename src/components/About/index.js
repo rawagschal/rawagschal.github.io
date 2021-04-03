028ee8e739e6d2dcfb2e09a834fc933f413f00be
Rachel Wagschal <rawagschal@gmail.com>
2021-04-02 17:08:34 -0700

    fixed navbar with react-router-dom

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,12 +5,10 @@ function About() {
     return (
         <section className="about-container">
             <div className="about-col-1">
-                <h2 id="about" className="about-header">Hello, World!</h2>
-                <p> 
-                    Meet Rachel, a junior full-stack web developer with
-                    a desire to create humane techonologies for more meaningful 
-                    engagement on the web. 
-                </p>
+                
+                <h3> 
+                    Creative coder, web developer, and proponent of the oxford comma. 
+                </h3>
             </div>
             <img src={aboutImage} className="about-img" alt="Rachel Wagschal on her graduation day from the University of Oregon in June 2019." />
         </section>
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,31 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
-// export const pages = [
-    //     {
-    //         id: "homepage",
-    //         name: "About"
-    //     },
-    //     {
-    //         id: "portfolio", 
-    //         name: "Portfolio",
-    //     },
-    //     {
-    //         id: "resume",
-    //         name: "Resume"
-    //     },
-    //     {
-    //         id: "contact",
-    //         name: "Contact"
-    //     }
-    // ];
-
-const AppNavbar = ({ currentPage, setCurrentPage }) => {
-    
-   function getClassName(page) {
-        return page === currentPage ? 'active' : '';
-    }
-
+const AppNavbar = () => {
     
     return (
         <>
@@ -36,35 +12,12 @@ const AppNavbar = ({ currentPage, setCurrentPage }) => {
                 </Navbar.Brand>
                 {/* future dev: responsive toggle */}
                 <Nav>
-                    <Nav.Link href='/' className={getClassName(page.id)}onClick={() => setCurrentPage(true)}>
-                        about
-                    </Nav.Link>
+                    {/* future dev: bold link on click/active */}
+                    <Nav.Link href='/'>about</Nav.Link>
                     <Nav.Link href='/portfolio'>my work</Nav.Link>
                     <Nav.Link href='/resume'>resume</Nav.Link>
                     <Nav.Link href='/contact'>get in touch</Nav.Link>
                 </Nav>
-                {/* <div className="navigation-items">
-                    <div className="navigation-wrap">
-                        <nav>
-                            {
-                                pages.map((page) => (
-                                    
-                                    // <a key={page.id} href={page.id} className="navigation-item" 
-                                    //     onClick={() => {
-                                    //         setCurrentPage(page.id);
-                                    //         document.title = page.name;
-                                    //         document.href = page.name;
-                                    //     }}
-                                    // >
-                                    //     {page.name}
-                                    // </a>
-                                    
-                                ))
-                            }
-                        </nav>
-                    </div>
-                </div> */}
-                
             </Navbar>
         </>
     );
