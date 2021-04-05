9ba471e0b1d47fcb9d3a3a6023c1d09233d3dd57
Rachel Wagschal <rawagschal@gmail.com>
2021-04-05 14:39:49 -0700

    got rid of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-// previously incl. { useState }
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-// previously: Navbar, { pages }
-import Navbar from './components/Navbar';
+import React, { useState } from 'react';
+import Navbar, { pages } from './components/Navbar';
 import About from './components/About';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
@@ -12,49 +9,46 @@ import Footer from './components/Footer';
 
 function App() {
   
-  // const [ currentPage, setCurrentPage ] = useState('homepage');
+  const [ currentPage, setCurrentPage ] = useState('homepage');
 
-  // let currentPageEl
+  let currentPageEl
   
-  // switch (currentPage) {
-  //   case pages[1].id:
-  //     currentPageEl = (
-  //     <Portfolio />
-  //     )
-  //     break; 
-  //   case pages[2].id:
-  //     currentPageEl = (
-  //       <Resume />
-  //     )
-  //     break; 
-  //   case pages[3].id:
-  //     currentPageEl = (
-  //       <Contact />
-  //     )
-  //     break; 
-  //   case pages[0].id:
-  //     default: currentPageEl = (
-  //       <>
-  //         <About />
-  //       </>
-  //     )
-  // }
+  switch (currentPage) {
+    case pages[1].id:
+      currentPageEl = (
+      <Portfolio />
+      )
+      break; 
+    case pages[2].id:
+      currentPageEl = (
+        <Resume />
+      )
+      break; 
+    case pages[3].id:
+      currentPageEl = (
+        <Contact />
+      )
+      break; 
+    case pages[0].id:
+      default: currentPageEl = (
+        <>
+          <About />
+        </>
+      )
+  }
 
   return (
-    <Router>
-      <>
-        <Navbar />
-          {/* currentPage={currentPage}
-          setCurrentPage={setCurrentPage} */}
-        <Switch>
-          <Route exact path='/' component={About} />
-          <Route exact path='/portfolio' component={Portfolio} />
-          <Route exact path='/resume' component={Resume} />
-          <Route exact path='/contact' component={Contact} />
-        </Switch>
+    <>
+      <Navbar 
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <main>
+        {currentPageEl}
+      
         <Footer/>
-      </>
-    </Router>
+      </main>
+    </>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,70 +1,54 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 
-// export const pages = [
-    //     {
-    //         id: "homepage",
-    //         name: "About"
-    //     },
-    //     {
-    //         id: "portfolio", 
-    //         name: "Portfolio",
-    //     },
-    //     {
-    //         id: "resume",
-    //         name: "Resume"
-    //     },
-    //     {
-    //         id: "contact",
-    //         name: "Contact"
-    //     }
-    // ];
-
-const AppNavbar = ({ currentPage, setCurrentPage }) => {
-    
-   function getClassName(page) {
-        return page === currentPage ? 'active' : '';
+export const pages = [
+    {
+        id: "homepage",
+        name: "About"
+    },
+    {
+        id: "portfolio",
+        name: "Portfolio"
+    },
+    {
+        id: "resume",
+        name: "Resume"
+    },
+    {
+        id: "contact",
+        name: "Contact"
     }
 
+];
+
+function AppNavbar({ currentPage, setCurrentPage }) {
+
+    function getClassName(page) {
+        return page === currentPage ? 'active' : ' ';
+    }
     
     return (
         <>
             <Navbar expand='lg' expanded='true'>
-                <Navbar.Brand as={Link} to='/'>
+                <Navbar.Brand href="/">
                     RACHEL WAGSCHAL
                 </Navbar.Brand>
                 {/* future dev: responsive toggle */}
                 <Nav>
-                    <Nav.Link href='/' className={getClassName(page.id)}onClick={() => setCurrentPage(true)}>
-                        about
-                    </Nav.Link>
-                    <Nav.Link href='/portfolio'>my work</Nav.Link>
-                    <Nav.Link href='/resume'>resume</Nav.Link>
-                    <Nav.Link href='/contact'>get in touch</Nav.Link>
+                    {
+                        pages.map((page) => (
+                            <Nav.Link key={page.id} className={getClassName(page.id)}
+                                onClick={() => {
+                                    setCurrentPage(page.id);
+                                    document.title = page.name;
+                            }}>
+                            {page.name}
+                            </Nav.Link>
+
+                        ))
+                    }
+                    
                 </Nav>
-                {/* <div className="navigation-items">
-                    <div className="navigation-wrap">
-                        <nav>
-                            {
-                                pages.map((page) => (
-                                    
-                                    // <a key={page.id} href={page.id} className="navigation-item" 
-                                    //     onClick={() => {
-                                    //         setCurrentPage(page.id);
-                                    //         document.title = page.name;
-                                    //         document.href = page.name;
-                                    //     }}
-                                    // >
-                                    //     {page.name}
-                                    // </a>
-                                    
-                                ))
-                            }
-                        </nav>
-                    </div>
-                </div> */}
-                
             </Navbar>
         </>
     );
